6641afc0b634cf56730d13ce8c7472db972ae48b
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-02-18 19:02:40 +0530

    create sign api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import UserModule from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-// import { errorHandle } from "../utils/error.js";
+import { errorHandle } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async function(req, res, next) {
     const {username, email, password} = req.body;
@@ -12,5 +13,20 @@ export const signUp = async function(req, res, next) {
     }catch(err) {
         next(err);
         // next(errorHandle(550, "error from the function"));
+    }
+};
+
+export const signIn = async (req, res , next) => {
+    const {email, password} = req.body;
+    try{
+        const validUser = await UserModule.findOne({email});
+        if(!validUser) return next(errorHandle(404, "user not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandle(401, "Wrong Credentials!"));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRECT);
+        const {password: pass, ...rest } = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+    }catch(error){
+        next(error);
     }
 }
