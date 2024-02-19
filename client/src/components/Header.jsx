009d9d7acacaaf66b973ee3f0f73c081d6ddced3
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-02-19 19:37:45 +0530

    update the header and protet the profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const {currentUser} = useSelector( state => state.user);
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -23,8 +25,12 @@ const Header = () => {
                     <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
                     </Link>
-                    <Link to='/sign-in'>
-                    <li className='sm:inline text-slate-700 hover:underline cursor-pointer'>Sign In</li>
+                    <Link to='/profile'>
+                    {currentUser ?  (
+                        <img src={currentUser.avatar} alt="profile" className='w-8 h-8 rounded-full border-2 border-black object-cover ml-2' />
+                    ): (
+                        <li className='sm:inline text-slate-700 hover:underline cursor-pointer'>Sign In</li>
+                    )}
                     </Link>
                 </ul>
             </div>
