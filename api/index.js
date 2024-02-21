ad135cb3ea9af2dd5806eee23ddf7e6051cd3e54
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-02-21 19:19:02 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 // initialize dotenv
 dotenv.config();
@@ -17,6 +18,7 @@ const app = express();
 
 // allow send json to the server
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, function() {
     console.log("server is running at port 3000");
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { test } from "../controllers/user.contorller.js";
+import { updateUser } from "../controllers/user.contorller.js";
+import { verifyToken } from "../utils/varifyUser.js";
 
 const router = express.Router();
 
 // write routes here
 
 router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
