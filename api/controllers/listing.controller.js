0f2d43e65cc9222e60e009d68b2aabf6c5b2a368
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-02-26 20:53:52 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -13,17 +13,17 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
-  if(!listing){
+  if (!listing) {
     return next(errorHandle(404, 'Listing not found'));
   }
 
-  if(req.user.id !== listing.userRef){
+  if (req.user.id !== listing.userRef) {
     return next(errorHandle(401, 'You can only delete your own listings'));
   }
-  try{
+  try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing has been deleted');
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 };
@@ -49,14 +49,60 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListing = async(req, res, next) => {
-  try{
+export const getListing = async (req, res, next) => {
+  try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing){
+    if (!listing) {
       return next(errorHandle(404, 'Listing not found'));
     }
     res.status(200).json(listing);
-  }catch(error){
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+      // it means if offer is undefind or false then we need to search inside the offer($in:), both offer are true and false from data base.
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listing = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      offer,
+      furnished,
+      parking,
+      type,
+    }).sort(
+      { [sort]: order }
+    ).limit(limit).skip(startIndex);
+
+    return res.status(200).json(listing);
+  } catch (error) {
     next(error);
   }
 }
