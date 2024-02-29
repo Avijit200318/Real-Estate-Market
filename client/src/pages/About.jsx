77decab68fc10f948937717892429b4300bbd9af
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-02-29 20:08:11 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4'>About Sahand Estate</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, id doloribus? Tenetur quas dolores omnis dicta non inventore quis quaerat officia ad voluptate reprehenderit, reiciendis corrupti eaque laboriosam magni dolorem.</p>
+
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque velit similique molestiae modi error expedita? Pariatur ipsam dignissimos inventore?</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda maxime nam eligendi esse harum tempora nobis! Architecto recusandae veniam assumenda velit labore sapiente facere rerum?</p>
     </div>
   )
 }
